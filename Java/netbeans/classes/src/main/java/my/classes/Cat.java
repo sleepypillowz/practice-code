4e178f63a9f23eed38f30c690e969b2728faaca2
Alexander Acojido <118715035+sleepypillowz@users.noreply.github.com>
2023-03-03 09:22:36 +0800

    Update Cat.java

--- Java/netbeans/classes/src/main/java/my/classes/Cat.java
+++ Java/netbeans/classes/src/main/java/my/classes/Cat.java
@@ -1,11 +1,16 @@
 package my.classes;
 
 public class Cat {
-    int tail = 1;
-    int eyes = 2;
-    String[] breeds = {"Persian", "Ragdoll", "birman"};
     
-    void printHi() {
+    int lives = 9;
+    int eyes = 2;
+    int tail = 1;
+        
+    void Meow() {
         System.out.println("meow");
+    }
+
+    void Sleep() {
+        System.out.println("zzz");
     }
 }
