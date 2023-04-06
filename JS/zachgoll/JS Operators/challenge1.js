b5919dacaa5c4943f74909fdd7abfda03cacb262
sleepypillowz <alexacojido@gmail.com>
2023-04-06 19:20:41 +0800

    Create challenge1.js

---
+++ JS/zachgoll/JS Operators/challenge1.js
@@ -0,0 +1,8 @@
+/* constant can't be changed thus we should use the let keyword which allows for change. */
+let numberVariable = 0;
+
+numberVariable++;
+numberVariable++;
+numberVariable++;
+
+console.log(numberVariable);
