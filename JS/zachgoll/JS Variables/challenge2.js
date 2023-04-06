1ebfc32f6dc536168ae4abf2637bc207f62c30de
sleepypillowz <alexacojido@gmail.com>
2023-04-06 19:07:28 +0800

    initial commit

---
+++ JS/zachgoll/JS Variables/challenge2.js
@@ -0,0 +1,9 @@
+/*
+ 1st error no semi colon (;)
+ 2nd error whitespace between variable
+ 3rd error redecleration of constant
+*/
+
+const someNumber = 20;
+
+someNumber = 50;
---
+++ JS/zachgoll/JS Variables/challenge3.js
@@ -0,0 +1,11 @@
+/*
+The resultVariable is equal to 2040
+The data type is a string
+*/
+
+const variable1 = 20;
+const variable2 = '40';
+
+const resultVariable = variable1 + variable2;
+
+console.log(resultVariable);
