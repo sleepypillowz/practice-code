3b8601c6c76bc67441dc0ef7b4d561865361e7a8
sleepypillowz <alexacojido@gmail.com>
2023-04-06 19:51:24 +0800

    Create challenge3.js

---
+++ JS/zachgoll/JS Operators/challenge3.js
@@ -0,0 +1,12 @@
+/* 
+expression5 evaluates to false 
+const result = !(((100 % 50) < (100 / 50)) && 300 + 5 === 305);
+*/
+
+const expression1 = 100 % 50;
+const expression2 = 100 / 50;
+const expression3 = expression1 < expression2;
+const expression4 = expression3 && 300 + 5 === 305;
+const expression5 = !expression4;
+
+console.log(expression5);
