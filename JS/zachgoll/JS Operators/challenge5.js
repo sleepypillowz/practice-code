002c88b73aee958d491f5490e99461e83e8e6df8
sleepypillowz <alexacojido@gmail.com>
2023-04-06 20:36:21 +0800

    Create challenge5.js

---
+++ JS/zachgoll/JS Operators/challenge5.js
@@ -0,0 +1,20 @@
+/* The result evaluates to true as 500 is > 300 */
+
+const myObj = {
+    nestedObject1: {
+        price: 100,
+        quantity: 5
+    },
+    nestedObject2: {
+        price: 150,
+        quantity: 2
+    }
+};
+
+const myArray = [myObj.nestedObject1, myObj.nestedObject2];
+
+const result =
+    (myArray[0].price * myArray[0].quantity) >
+    (myArray[1].price * myArray[1].quantity);
+
+console.log(result);
