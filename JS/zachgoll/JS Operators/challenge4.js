9622bbe60135f0ad507508b19642c8b8b09eb760
sleepypillowz <alexacojido@gmail.com>
2023-04-06 19:56:44 +0800

    Create challenge4.js

---
+++ JS/zachgoll/JS Operators/challenge4.js
@@ -0,0 +1,12 @@
+/* result evaluates to true */
+
+const myObj = {
+    prop1: 'first value',
+    prop2: 20
+};
+
+const myArray = [40, 50, 2];
+
+const result = myObj.prop2 == (myArray[0] / myArray[2]);
+
+console.log(result);
