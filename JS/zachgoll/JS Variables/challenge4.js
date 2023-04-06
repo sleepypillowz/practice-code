be6b43c253f810624fb71f9c97750524529ec0bb
sleepypillowz <alexacojido@gmail.com>
2023-04-06 19:14:53 +0800

    Create challenge4.js

---
+++ JS/zachgoll/JS Variables/challenge4.js
@@ -0,0 +1,9 @@
+/* The Code is Invalid because the comma's is replaced with a semi colon*/
+
+const objectVariable = {
+    property1: 'i am property 1',
+    property2: 'i am property 2',
+    property3: [20, 30, 40],
+};
+
+console.log(objectVariable.property3[2]);
