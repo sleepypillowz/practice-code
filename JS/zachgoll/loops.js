26a8c2ea48800c9f1663ab0361777ee3716935fb
sleepypillowz <alexacojido@gmail.com>
2023-04-07 12:43:05 +0800

    Create loops.js

---
+++ JS/zachgoll/loops.js
@@ -0,0 +1,34 @@
+const blogPosts = [
+    {
+        title: 'What is JavaScript?',
+        author: 'Zach Gollwitzer',
+        publishDate: 'Dec 20, 2020',
+        content: 'some post content here'
+    },
+    {
+        title: 'How do Arrays work??',
+        author: 'Zach Gollwitzer',
+        publishDate: 'Jan 1, 2021',
+        content: 'some post content here'        
+    },
+    {
+        title: 'How long does it take to learn coding??',
+        author: 'Zach Gollwitzer',
+        publishDate: 'Jan 20, 2021',
+        content: 'some post content here'        
+    }
+]
+
+for (let i = 0; i < blogPosts.length; i++) {
+    const postTitle = blogPosts[i].title;
+    const postAuthor = blogPosts[i].author;
+    const postDate = blogPosts[i].publishDate;
+    const postContent = blogPosts[i].content;
+
+    //Here we would use these variables to do something with each post
+    // I'll just print the values
+    console.log(postTitle);
+    console.log(postAuthor);
+    console.log(postDate);
+    console.log(postContent);
+}
