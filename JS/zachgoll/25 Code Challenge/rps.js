a4db0c89186efeaa4d3f09dfa288c349a0764f94
sleepypillowz <alexacojido@gmail.com>
2023-04-08 10:37:32 +0800

    initial commit

--- JS/zachgoll/25 Code Challenge/rps.js
+++ JS/zachgoll/25 Code Challenge/rps.js
@@ -5,28 +5,26 @@ const rps = (p1, p2) => {
     const P2W = 'Player 2 won!';
 
     for (let i = 0; i < choice.length; i++) {
-        if (p1 === choice[i]) {
-            if (p2 === choice[i]) {
-                return 'Draw!';
-            }
-            else if (p1 === choice[0] && p2 === choice[2]) {
-                return P1W;
-            }
-            else if (p1 === choice[1] && p2 === choice[0]) {
-                return P1W;
-            }
-            else if (p1 === choice[2] && p2 === choice[1]) {
-                return P1W;
-            }
-            else if (p2 === choice[0] && p1 === choice[2]) {
-                return P2W;
-            }
-            else if (p2 === choice[1] && p1 === choice[0]) {
-                return P2W;
-            }
-            else if (p2 === choice[2] && p1 === choice[1]) {
-                return P2W;
-            }
+        if (p1 === p2) {
+            return 'Draw!';
+        }
+        else if (p1 === choice[0] && p2 === choice[2]) {
+            return P1W;
+        }
+        else if (p1 === choice[1] && p2 === choice[0]) {
+            return P1W;
+        }
+        else if (p1 === choice[2] && p2 === choice[1]) {
+            return P1W;
+        }
+        else if (p2 === choice[0] && p1 === choice[2]) {
+            return P2W;
+        }
+        else if (p2 === choice[1] && p1 === choice[0]) {
+            return P2W;
+        }
+        else if (p2 === choice[2] && p1 === choice[1]) {
+            return P2W;
         }
     }
 };
