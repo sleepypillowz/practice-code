18f90e4c857b4a53190bd6cc9c918db8915558b6
sleepypillowz <alexacojido@gmail.com>
2023-09-04 00:00:52 +0800

    removed and moved

--- HTML/sti28/assets/template/hello.md
+++
@@ -1,5 +0,0 @@
-Joe! hello world!
-
-hjkl - navigation
-iIaA - editing in line
-xr - making changes while in command mode
--- HTML/sti28/chat/connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$connect = mysqli_connect("localhost", "root", "", "realtimelocatordb", 3307) or die ("ERROR");
-#echo "trial";
-?>
