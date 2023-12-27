638cb1975957b306edfcc4c5853e042514edc1f1
Alexander Acojido <alexacojido@gmail.com>
2023-12-27 18:45:27 +0800

    added components
    home.js
    navbar.js

--- JS/React/dojo-blog/src/App.js
+++ JS/React/dojo-blog/src/App.js
@@ -1,22 +1,13 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  const link = "http://www.google.com";
-
   return (
     <div className="App">
-      <div className="conteent"></div>
-      <h1>{ title }</h1>
-      <p>Liked { likes } times </p>
-
-      <p>{ 10 }</p>
-      <p>{ "hello, ninjas" }</p>
-      <p>{ [1,2,3,4,5] }</p>
-      <p>{ Math.random() * 10 }</p>
-
-      <a href={ link }>Google Site</a>
+      <Navbar />
+      <div className="content"></div>
+        <Home />
     </div>
   );
 }
