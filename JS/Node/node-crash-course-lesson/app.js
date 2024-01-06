0a0b40a92b597b41873b21d833bdcbcedc8be463
Alexander Acojido <alexacojido@gmail.com>
2024-01-06 18:11:58 +0800

    added get,post,del req

--- JS/Node/node-crash-course-lesson/app.js
+++ JS/Node/node-crash-course-lesson/app.js
@@ -17,6 +17,7 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded( {extended: true} ));
 app.use(morgan('dev'));
 
 
@@ -39,6 +40,41 @@ app.get('/blogs', (req, res) => {
       console.log(err);
     })
 });
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog.save()
+    .then((result) => {
+      res.redirect('/blogs');
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+})
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then(result => {
+      res.render('details', { blog: result, title: 'Blog Details' })
+    })
+    .catch(err => {
+      console.log(err);
+    });
+})
+
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({ redirect: '/blogs' })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
 
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
