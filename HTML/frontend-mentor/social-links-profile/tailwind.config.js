97a5092d307cc2bf061f4eee9f6291f46167366d
Alexander Acojido <alexacojido@gmail.com>
2024-01-20 14:45:49 +0800

    finalized design

--- HTML/frontend-mentor/social-links-profile/tailwind.config.js
+++ HTML/frontend-mentor/social-links-profile/tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["./src/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: 'hsl(75, 94%, 57%)',
+        grey: `hsl(0, 0%, 20%)`,
+        'dark-grey': 'hsl(0, 0%, 12%)',
+        'off-black': 'hsl(0, 0%, 8%)',
+      },
+      fontFamily: {
+        body: ['Inter']
+      }
+    },
   },
   plugins: [],
-}
-
+};
