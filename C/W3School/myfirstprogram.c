4c832af21cd6324f41847bc542ed4d4af5043e0c
Alexander Acojido <alexacojido@gmail.com>
2024-04-23 22:45:35 +0800

    update

--- C/W3School/myfirstprogram.c
+++ C/W3School/myfirstprogram.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hello World\n");
-  printf("I am learning C.\n");
-  printf("\t indented paragraph here\n");
-  printf("\\\\ This is how you write a comment.\n");
-  printf("\"Wow\"\n"); // This is a comment
-  // I am also a comment
-  /* This is a multi-line
-     comment*/
+  printf("Hello World!");
   return 0;
-}
+} 
