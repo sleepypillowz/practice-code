9043cc898a529126e42f0935813492184324631a
Alexander Acojido <alexacojido@gmail.com>
2024-05-20 01:16:50 +0800

    update

--- Unity/KitchenChaos/Assets/Scripts/Player.cs
+++ Unity/KitchenChaos/Assets/Scripts/Player.cs
@@ -2,37 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Player : MonoBehaviour {
 
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameinput;
+    private bool isWalking;
 
     private void Update() {
-
-        // Movement
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.W)) {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            inputVector.x = +1;
-        }
-
-        // Removes Fast Diagonal Movement
-        inputVector = inputVector.normalized;
-
+        Vector2 inputVector = gameinput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
         // Rotate Character towards move direction and Lerp makes it smooth
+        isWalking = moveDir != Vector3.zero;
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking() {
+        return isWalking;
     }
 
 }
